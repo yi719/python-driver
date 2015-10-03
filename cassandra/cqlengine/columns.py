2bfa294e70f36f072bf02638cceb9570d480c1e3
yi719 <emotional_etc@msn.com>
2015-10-03 21:25:34 +0800

    Fix datetime when user msgpack

--- cassandra/cqlengine/columns.py
+++ cassandra/cqlengine/columns.py
@@ -913,12 +913,12 @@ class _PartitionKeysToken(Column):
 
 def decode_datetime(obj):
     if b'__datetime__' in obj:
-        obj = datetime.datetime.strptime(obj["as_str"], "%Y%m%dT%H:%M:%S.%f")
+        obj = datetime.strptime(obj["as_str"], "%Y%m%dT%H:%M:%S.%f")
     return obj
 
 
 def encode_datetime(obj):
-    if isinstance(obj, datetime.datetime):
+    if isinstance(obj, datetime):
         return {'__datetime__': True, 'as_str': obj.strftime("%Y%m%dT%H:%M:%S.%f")}
     return obj
 
